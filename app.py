85386f6bd128e356c45e4e589b5173aeae6ee35e
HFY-gif <142511450+HFY-gif@users.noreply.github.com>
2025-07-02 11:09:44 +0800

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from extensions import app, db
-from routes import *
-
-if __name__ == '__main__':
-    from models import User, PlayRecord
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
