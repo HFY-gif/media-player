a2d7b8dffa01357337aa5fc5fb0a12abb48c03ff
HFY-gif <142511450+HFY-gif@users.noreply.github.com>
2025-07-02 11:09:32 +0800

    Delete extensions.py

--- extensions.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:hfy201612@localhost:3306/media-player'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = '123456'
-
-db = SQLAlchemy(app)
